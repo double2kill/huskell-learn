27771a6560f877e9e62cf25f380fb422df2cff13
double2kill <379563000@qq.com>
2021-09-18 08:47:56 +0800

    feat: 🎸 Add a http server

--- app/Main.hs
+++ app/Main.hs
@@ -1,22 +1,21 @@
+{-# LANGUAGE DataKinds, TypeOperators, OverloadedStrings #-}
 module Main where
 
-import qualified MyFunction
-import qualified MyLib
-import qualified MyList
-import qualified ReadFile
-import qualified DataList
+import qualified Data.Text    as T
+import qualified Data.Text.Lazy.Encoding as E
+import Network.Wai ( responseLBS, Application )
+import Network.HTTP.Types ( status200 )
+import Network.Wai.Handler.Warp (run)
 
-add :: Integer -> Integer -> Integer
+app :: Application
+app _ respond = do
+    putStrLn "I've done some IO here"
+    respond $ responseLBS
+        status200 
+        [("Content-type","application/json; charset=utf-8")] 
+        (E.encodeUtf8 "Hello 刘晨!")
+
 main :: IO ()
-
-add x y = x + y
-
-doubleMe x = x + x 
-
 main = do
-  putStrLn "Hello, Haskell!"
-  MyFunction.main
-  MyLib.hello
-  MyList.main
-  ReadFile.main
-  DataList.main
+    putStrLn $ "http://localhost:3000/"
+    run 3000 app
--- learning_log.md
+++ learning_log.md
@@ -4,3 +4,4 @@ http://learnyouahaskell.com/chapters
 # Step 2: 读文件和写文件
 
 # Step 3: 学习常用的库
+2021-09-17 21:39:04 wai: 启动一个http-server
